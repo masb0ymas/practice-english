31362f198d77edbee08a1b3544ddc2450ca43cc6
masb0ymas <n.fajri@outlook.com>
2025-09-25 16:02:32 +0700

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import viteTsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
-import { nitro } from 'nitro/vite'
+import { nitro } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   server: {
